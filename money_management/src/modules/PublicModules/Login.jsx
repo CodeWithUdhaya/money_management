5ae24c6de5306158d975d8a5434223764de01f4f
udhayakumar2701 <116900847+udhayakumar2701@users.noreply.github.com>
2025-10-30 23:14:06 +0530

    change in login js

--- money_management/src/modules/PublicModules/Login.jsx
+++ money_management/src/modules/PublicModules/Login.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import {React,useState} from "react";
 import "./Login.scss";
+import { loginApi } from '../../ApiUtils/Api';
+
+ 
 
 const Login = () => {
+
+   const [username,setUsername]= useState("");
+   const [password,setPassword]= useState("");
+   const [loading,isLoading]= useState(false);
+
+   const handleLogin = async (e) =>{
+         e.preventDefault();
+        isLoading(true);
+          const data = await loginApi({
+                "username": username,
+                "password": password
+               })
+            console.log("dashboard "+data); 
+            isLoading(false) ;
+  }
+
+   const handleUsername = (e) => {
+  setUsername(e.target.value);
+};
+   const handlePassword =(e) =>{
+    setPassword(e.target.value);
+   }
+
   return (
     <div className="login-page">
+      {isLoading && <p>Loading.........</p>}
       <div className="login-container">
         <h1>Money Management</h1>
         <form>
           <div className="input-group">
-            <input type="text" placeholder="Username/email" required />
+            <input type="text" placeholder="Username/email" onChange={handleUsername} required />
           </div>
           <div className="input-group">
-            <input type="password" placeholder="Password" required />
+            <input type="password" placeholder="Password" onChange={handlePassword}required />
           </div>
-          <button type="submit" className="login-btn">Login</button>
+          <button type="submit" className="login-btn" onClick={handleLogin}>Login</button>
           <a href="#" className="register-link">Register</a>
         </form>
       </div>
